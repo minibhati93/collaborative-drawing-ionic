d2ef8a0aac86ad273caf7c4c30882fda07ec401e
minibhati93 <minibhati93@gmail.com>
2016-07-20 20:14:01 +0530

    Google oauth added

--- www/js/controllers/HomeController.js
+++ www/js/controllers/HomeController.js
@@ -1,8 +1,8 @@
 (function(){
   angular.module('starter')
-  .controller('HomeController', ['$scope', '$ionicPopover' ,'$state', HomeController]);
+  .controller('HomeController', ['$scope', '$ionicPopover', '$http' ,'$state', '$ionicModal', HomeController]);
   
-  function HomeController($scope, $ionicPopover ,$state){
+  function HomeController($scope, $ionicPopover, $http ,$state, $ionicModal){
 
          $scope.tools = { strokeSize : '20' , strokeColor: '#000000'};
  
@@ -185,11 +185,63 @@
         /************************** Save Canvas **********************************/
  
         $scope.saveDrawing = function(){
-            if(!window.localStorage.getItem("username")){
-                 alert("Please Log-in");
-                 $state.go('login');
+            if(!window.localStorage.getItem("uuid")){
+                 // alert("Please Log-in");
+                 $scope.login();
             }
         };
+ 
+ 
+ 
+        $ionicModal.fromTemplateUrl('templates/login-modal.html', {
+            scope: $scope,
+            backdropClickToClose: false
+         }).then(function(modal) {
+             $scope.modalA = modal;
+         });
+ 
+ 
+         // Triggered in the login modal to close it
+        $scope.closeLogin = function() {
+            $scope.modalA.hide();
+        };
+         
+         // Open the login modal
+        $scope.login = function() {
+            $scope.modalA.show();
+        };
+ 
+ 
+        $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+         // var clientId = "129939593061-2jvlmucpf0pk2rng15n518v68med5vta.apps.googleusercontent.com";
+        var requestToken = "";
+        var accessToken = "";
+        var clientId = "129939593061-2jvlmucpf0pk2rng15n518v68med5vta.apps.googleusercontent.com";
+        var clientSecret = "QL50fc0sEZEnvJi7DH74urL2";
+ 
+        $scope.googleOauth = function(){
+             var ref = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=https://www.googleapis.com/auth/userinfo.email&approval_prompt=force&response_type=code&access_type=offline', '_blank', 'location=no');
+             ref.addEventListener('loadstart', function(event) {
+                      if((event.url).startsWith("http://localhost/callback")) {
+                      requestToken = (event.url).split("code=")[1];
+                      $http({method: "post", url: "https://accounts.google.com/o/oauth2/token", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=http://localhost/callback" + "&grant_type=authorization_code" + "&code=" + requestToken })
+                      .success(function(data) {
+                               accessToken = data.access_token;
+                               $scope.verifyToken(accessToken);
+                               })
+                      .error(function(data, status) {
+                             alert("ERROR: " + data);
+                             });
+                      ref.close();
+                      }
+                      });
+        };
+ 
+ 
+        $scope.verifyToken = function(accessToken){
+             window.localStorage['uuid'] = encodeURIComponent(accessToken);
+        };
+
 
  
  
